32f8804609d365635cfd55fece3d9d761a0007ba
jzcurious <paranoid.hpc.io@gmail.com>
2023-09-16 17:03:46 +0700

    add lab8

---
+++ lab8/lab8.py
@@ -0,0 +1,88 @@
+import torch
+from torch import nn
+import torch.optim as optim
+from tqdm import tqdm
+from torch.cuda.amp import GradScaler
+from ..lab3.lab3 import LinearFunction
+from ..lab5.lab5 import (
+    MyNet,
+    prepare_cifar10,
+    test,
+    eval_valid_set_accuracy,
+    test_accuracy
+)
+
+
+def train_iteration(
+    train_data, test_data,
+    net, epochs,
+    lr=0.005, device='cuda:0',
+    bar_label='', prev_epochs=0,
+    best_valid_accuracy=0.0,
+    use_mixed=True
+):
+    net.to(device)
+
+    grad_scaler = GradScaler()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
+
+    total_epochs = prev_epochs + epochs
+    train_accuracy = torch.zeros((epochs,), device=device)
+    valid_accuracy = torch.zeros((epochs,), device=device)
+
+    if not bar_label:
+        bar_label = 'Train'
+
+    for e in range(0, epochs):
+        with tqdm(train_data, unit='batch') as epoch:
+            bar_epoch = prev_epochs + e + 1
+
+            epoch.set_description(
+                f'{bar_label}. Epoch {bar_epoch}/{total_epochs}')
+
+            for i, (inputs, labels) in enumerate(epoch):
+                x, y = inputs.to(device), labels.to(device)
+
+                optimizer.zero_grad()
+
+                with torch.autocast(enabled=use_mixed,
+                                    device_type='cuda', dtype=torch.float16):
+
+                    y_ = net(x)
+                    loss = criterion(y_, y)
+
+                grad_scaler.scale(loss).backward()
+                grad_scaler.step(optimizer)
+                grad_scaler.update()
+
+                train_accuracy[e] += (
+                    test_accuracy(y_, y, len(train_data))
+                )
+
+                epoch.set_postfix(loss=loss.item())
+
+            valid_accuracy[e] = \
+                eval_valid_set_accuracy(net, test_data, device).item()
+
+            if best_valid_accuracy < valid_accuracy[e]:
+                best_valid_accuracy = valid_accuracy[e]
+                torch.save(net.state_dict(), 'best_model.pt')
+
+    return train_accuracy, valid_accuracy, best_valid_accuracy
+
+
+if __name__ == '__main__':
+    LinearFunction.up_backend('hs/lab7/lab7.cu')
+
+    train_data, test_data = prepare_cifar10()
+    net = MyNet(num_classes=10)
+
+    train_iteration(train_data, test_data, net, epochs=1, lr=0.02)
+
+    valid_accuracy_cls = test(net)
+
+    print('\nAccuracy (valid)')
+    print(f'  min: {valid_accuracy_cls.min().item():.0f}%')
+    print(f'  avg: {valid_accuracy_cls.mean().item():.0f}%')
+    print(f'  max: {valid_accuracy_cls.max().item():.0f}%\n')
